1ff3a4134dd52f4177a719ddb9d1afc162482bb5
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-03 17:34:25 -0300

    adicionando validações de casos

--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const casosController = require("../controllers/casosController");
+const {
+  newCasoValidation,
+  updateCasoValidation,
+  partialUpdateCasoValidation,
+} = require("../utils/casosValidations");
 
 router.get("/casos/search", casosController.filter);
 router.get("/casos/:caso_id/agente", casosController.getAgenteByCasoId);
 router.get("/casos/:id", casosController.getCasosById);
 router.get("/casos", casosController.getAllCasos);
-router.post("/casos", casosController.createCaso);
-router.put("/casos/:id", casosController.updateCaso);
-router.patch("/casos/:id", casosController.updatePartialCaso);
+router.post("/casos", newCasoValidation, casosController.createCaso);
+router.put("/casos/:id", updateCasoValidation, casosController.updateCaso);
+router.patch(
+  "/casos/:id",
+  partialUpdateCasoValidation,
+  casosController.updatePartialCaso
+);
 router.delete("/casos/:id", casosController.deleteCaso);
 
 module.exports = router;
