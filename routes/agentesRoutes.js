93a91aab2294ebad3baae25c5e1deec22d5dfac6
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-03 17:32:10 -0300

    adicionando validações de agentes

--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const agentesController = require("../controllers/agentesController");
+const {
+  newAgenteValidation,
+  updateAgenteValidation,
+  partialUpdateAgenteValidation,
+} = require("../utils/agentesValidations");
 
 router.get("/agentes/:id", agentesController.getAgenteById);
 router.get("/agentes", agentesController.getAllAgentes);
-router.post("/agentes", agentesController.createAgente);
-router.put("/agentes/:id", agentesController.updateAgente);
-router.patch("/agentes/:id", agentesController.updatePartialAgente);
+router.post("/agentes", newAgenteValidation, agentesController.createAgente);
+router.put(
+  "/agentes/:id",
+  updateAgenteValidation,
+  agentesController.updateAgente
+);
+router.patch(
+  "/agentes/:id",
+  partialUpdateAgenteValidation,
+  agentesController.updatePartialAgente
+);
 router.delete("/agentes/:id", agentesController.deleteAgente);
 
 module.exports = router;
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -14,8 +14,6 @@ const validate = (schema, req) => {
   }
 };
 
-module.exports = validate;
-
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Erro interno no servidor";
