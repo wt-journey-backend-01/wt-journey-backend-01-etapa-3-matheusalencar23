48c5d06e2d3312dc11031033254481215fedc7c2
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-04 21:31:06 -0300

    ajustando migrations

--- db/migrations/20250801194155_solution_migrations.js
+++ db/migrations/20250801194155_solution_migrations.js
@@ -2,32 +2,33 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema
-    .createTable("agentes", function (table) {
-      table.increments("id").primary();
-      table.string("nome").notNullable();
-      table.date("dataDeIncorporacao").notNullable();
-      table.string("cargo").notNullable();
-    })
-    .createTable("casos", function (table) {
-      table.increments("id").primary();
-      table.string("titulo").notNullable();
-      table.text("descricao").notNullable();
-      table.enu("status", ["aberto", "solucionado"]);
-      table
-        .integer("agente_id")
-        .references("id")
-        .inTable("agentes")
-        .notNullable()
-        .onDelete("CASCADE");
-    });
+exports.up = async function (knex) {
+  await knex.schema.createTable("agentes", function (table) {
+    table.increments("id").primary();
+    table.string("nome").notNullable();
+    table.date("dataDeIncorporacao").notNullable();
+    table.string("cargo").notNullable();
+  });
+
+  await knex.createTable("casos", function (table) {
+    table.increments("id").primary();
+    table.string("titulo").notNullable();
+    table.text("descricao").notNullable();
+    table.enum("status", ["aberto", "solucionado"]);
+    table
+      .integer("agente_id")
+      .references("id")
+      .inTable("agentes")
+      .notNullable()
+      .onDelete("CASCADE");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("casos").dropTableIfExists("agentes");
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("casos");
+  await knex.dropTableIfExists("agentes");
 };
